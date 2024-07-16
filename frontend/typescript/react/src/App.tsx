3ecae089efb125ed0f818ed2858dda5cdae4dc2d
Alex Patterson <alex.patterson@fusionauth.io>
2024-07-16 14:45:39 -0400

    adjust footer

--- frontend/typescript/react/src/App.tsx
+++ frontend/typescript/react/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">&copy; 2024 TODO_2. All rights reserved.</p>
+        <p className="text-xs text-muted-foreground">&copy; 2024 FusionAuth TODO. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <a href="#" className="text-xs hover:underline underline-offset-4">
             Terms of Service
